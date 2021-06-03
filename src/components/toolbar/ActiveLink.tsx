afa7aec9cf0bb043fef671283f31c5eb55b2c2a3
Vitor DevSP <contatodevsp@gmail.com>
2021-06-02 23:03:41 -0300

    feat: criando componente ActiveLink

---
+++ src/components/toolbar/ActiveLink.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from 'next/router'
+import Link, { LinkProps } from 'next/link'
+import { Link as LinkChakra } from '@chakra-ui/react'
+
+interface ActiveLinkProps extends LinkProps {
+  children: string
+}
+
+export function ActiveLink({ children, ...rest }: ActiveLinkProps) {
+  const { asPath } = useRouter()
+
+  const isActiveLink = asPath === rest.href
+
+  return (
+    <Link {...rest}>
+      <LinkChakra
+        fontSize="lg"
+        opacity={isActiveLink ? 1 : 0.7}
+        color={isActiveLink ? 'custom.primary' : 'gray.50'}
+        fontWeight={isActiveLink ? 'semibold' : 'normal'}
+        _hover={{ opacity: 1 }}
+      >
+        {children}
+      </LinkChakra>
+    </Link>
+  )
+}
