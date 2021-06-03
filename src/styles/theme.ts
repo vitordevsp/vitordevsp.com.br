464fd2bf9fb48c1913155e16d07d5acb7d482931
Vitor DevSP <contatodevsp@gmail.com>
2021-06-02 22:57:13 -0300

    style(theme.ts): arrumando estilo do codigo

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,7 +6,7 @@ export const theme = extendTheme({
       body: {
         bg: 'gray.900',
         color: 'gray.50',
-      }
+      },
     },
   },
   fonts: {
@@ -15,15 +15,15 @@ export const theme = extendTheme({
   },
   colors: {
     custom: {
-      'primary': '#7489F4',
-      'secondary': '#FF0083',
+      primary: '#7489F4',
+      secondary: '#FF0083',
     },
     gray: {
-      '50': '#F8F8F8',
-      '100': '#D7D7D7',
-      '700': '#2D3B59',
-      '800': '#212B41',
-      '900': '#0E141B',
+      50: '#F8F8F8',
+      100: '#D7D7D7',
+      700: '#2D3B59',
+      800: '#212B41',
+      900: '#0E141B',
     },
-  }
+  },
 })
