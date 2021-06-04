df9c0f48491ad5944f553cfcee7062cde6c6a9fb
Vitor DevSP <contatodevsp@gmail.com>
2021-06-03 21:15:34 -0300

    feat: criando componente CardInfo

---
+++ src/components/CardInfo/index.tsx
@@ -0,0 +1,31 @@
+import { Box, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react'
+
+import { Badge } from '../Badge'
+
+interface CardInfoProps {
+  src: string
+  badges?: string[]
+  title: string
+  description: string
+}
+
+export function CardInfo({ src, badges, title, description }: CardInfoProps) {
+  return (
+    <Stack spacing={2.5}>
+      <Image w="300px" h="168px" borderRadius="lg" src={src} alt="project" />
+
+      {badges && (
+        <Flex gridGap={2} wrap="wrap">
+          {badges.map((badge, i) => (
+            <Badge key={i}>{badge}</Badge>
+          ))}
+        </Flex>
+      )}
+
+      <Box>
+        <Heading as="h3" fontSize="3xl">{title}</Heading>
+        <Text color="gray.100">{description}</Text>
+      </Box>
+    </Stack>
+  )
+}
