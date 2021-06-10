c6929022ec1d6aaa25fa5d9eb1ba5e0602068257
Vitor DevSP <contatodevsp@gmail.com>
2021-06-10 17:01:52 -0300

    feat: add favicon/title window

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Box, ChakraProvider } from '@chakra-ui/react'
 
 import { Toolbar } from '../components/toolbar'
@@ -9,6 +10,10 @@ import '../styles/scrollbar.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Vitor DevSP</title>
+      </Head>
+
       <Box maxW="1168px" mx="auto" px={6}>
         <Toolbar />
         <Component {...pageProps} />
