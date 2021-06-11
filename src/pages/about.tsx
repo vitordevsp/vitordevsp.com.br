c3296f6b41200fd52a6737b81e5a1d9141082b9f
Vitor DevSP <contatodevsp@gmail.com>
2021-06-10 21:28:43 -0300

    style(page/about): modificando o texto da section sobre

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -11,43 +11,45 @@ export default function About() {
 
         <Stack spacing={4}>
           <Text>
-            São Paulo, SP, Brasil
+            São Paulo, SP, Brasil.
           </Text>
 
           <Text>
-            Formado no Ensino médio integrado com Técnico em informática, minha história com a
-            programação se inicia em 2014, comecei aprendendo lógica de programação e a criar
-            algoritmos com o VisualG, depois passei por desenvolvimento de software com Java e C#.
-            No último ano ao iniciar o meu TCC, decidi me desafiar a criar algo diferente do que
-            estava na grade disciplinar, passei a estudar Android por conta própria e desenvolvi
-            um App chamado mochila do aluno, que ajudaria o aluno a organizar sua vida acadêmica.
+            Formado no Ensino Médio Integrado com Técnico em Informática, minha história com a
+            programação se inicia em 2014. Comecei aprendendo lógica de programação e a criar
+            algoritmos com o VisualG e depois passei por desenvolvimento de software com Java e C#.
+            No último ano, ao iniciar o meu TCC, decidi me desafiar a criar algo diferente do que
+            estava na grade disciplinar. Assim, passei a estudar Android por conta própria e
+            desenvolvi um App chamado &quot;mochila do aluno&quot;, com o objetivo de ajudar o aluno
+            a organizar sua vida acadêmica.
           </Text>
 
           <Text>
-            Ao sair do Ensino médio, decidi que trabalharia por conta própria, pouco tempo depois
+            Ao sair do Ensino Médio, decidi que trabalharia por conta própria e pouco tempo depois
             comecei a trabalhar com Desenvolvimento Web e a fazer Freelas. Com isso,
-            passei a desenvolver o layout dos projetos que iria trabalhar no Figma,
+            passei a desenvolver o layout dos projetos que iria trabalhar no Figma
             e me apaixonei também pela área de UI Design.
           </Text>
 
           <Text>
             Passei o ano de 2020 e parte de 2021 trabalhando na empresa do Igor Oliveira,
-            do canal Programador BR, que conta com mais de 130k de inscritos no YouTube.
-            Fui o responsável pelo suporte do curso de desenvolvimento web full stack por certa de
-            1 ano e isso me ajudou muito a me desenvolver.
+            do canal Programador BR, que conta com mais de 140k de inscritos no YouTube.
+            Lá fui o responsável pelo suporte do curso de desenvolvimento web full stack
+            por certa de 1 ano e isso me ajudou muito a me desenvolver.
           </Text>
 
           <Text>
-            Nesse período lidei com diversas perguntas e e-mails com duvidas dos alunos sobre as
-            tecnologias da web e pude evoluir muito me aprofundando para responder sempre da melhor
-            maneira possível, me tornei um especialista em achar erro no código de outras pessoas.
+            Nesse período lidei com diversas perguntas e e-mails com dúvidas dos alunos sobre as
+            tecnologias da web e pude evoluir muito me aprofundando para responder sempre da
+            melhor maneira possível. Graças a essa experiência, me tornei um especialista em
+            achar erro no código de outras pessoas.
           </Text>
 
           <Text>
-            Sempre busco me aperfeiçoar, e estudo constantemente para isso,
-            atualmente estou me aventurando em criar conteúdo, para o YouTube e o Medium.
-            Acredito que a melhor maneira de aprender é ensinando, estou tentando transmitir um pouco
-            do meu conhecimento enquanto vou lapidando ele.
+            Sempre busco me aperfeiçoar e estudo constantemente para isso. Atualmente estou me
+            aventurando em criar conteúdo para o YouTube e o Dev.To. Faço isso pois acredito que
+            a melhor maneira de aprender é ensinando, e dessa forma transmito um pouco do meu
+            conhecimento enquanto vou lapidando ele.
           </Text>
 
           <Text>
