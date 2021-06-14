8d80f7f227ad319ce9e618a814256e36e02a9b08
Vitor DevSP <contatodevsp@gmail.com>
2021-06-14 13:18:03 -0300

    feat: responsividade - comp. CardInfoLarge

--- src/components/CardInfoLarge/index.tsx
+++ src/components/CardInfoLarge/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Icon, Image, Stack, Text } from '@chakra-ui/react'
+import { AspectRatio, Box, Flex, Heading, Icon, Image, Stack, Text } from '@chakra-ui/react'
 import { FiExternalLink } from 'react-icons/fi'
 
 import { Badge } from '../Badge'
@@ -15,33 +15,35 @@ export function CardInfoLarge({ src, title, badges, description, href }: CardInf
   return (
     <Stack
       as="a"
-      maxW="800px"
-      direction="row"
+      maxW={['380px', '380px', '800px']}
+      direction={['column', 'column', 'row']}
       spacing={3}
       href={href || undefined}
       target="_blank"
       role={href ? 'group' : undefined}
     >
-      <Box minW="300px" h="170px" borderRadius="lg" position="relative" overflow="hidden">
-        <Image minW="100%" h="100%" src={src} alt="project" />
+      <AspectRatio minW={['270px', '360px']} h="fit-content" ratio={16 / 9} >
+        <Box h="fit-content" borderRadius="lg" position="relative" overflow="hidden">
+          <Image src={src} alt="project" />
 
-        {href && (
-          <Flex
-            w="100%"
-            h="100%"
-            bgColor="rgba(0, 0, 0, 0.7)"
-            position="absolute"
-            top="0"
-            align="center"
-            justify="center"
-            opacity="0"
-            transition="opacity 0.3s"
-            _groupHover={{ opacity: '1' }}
-          >
-            <Icon as={FiExternalLink} w={8} h={8} />
-          </Flex>
-        )}
-      </Box>
+          {href && (
+            <Flex
+              w="100%"
+              h="100%"
+              bgColor="rgba(0, 0, 0, 0.7)"
+              position="absolute"
+              top="0"
+              align="center"
+              justify="center"
+              opacity="0"
+              transition="opacity 0.3s"
+              _groupHover={{ opacity: '1' }}
+            >
+              <Icon as={FiExternalLink} w={8} h={8} />
+            </Flex>
+          )}
+        </Box>
+      </AspectRatio>
 
       <Stack spacing={2.5}>
         <Heading
