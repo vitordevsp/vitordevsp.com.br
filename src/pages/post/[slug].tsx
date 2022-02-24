d888510ca03351e491a099c1497520560c0625c4
Vitor DevSP <contatodevsp@gmail.com>
2022-02-24 20:28:21 -0300

    feat: criando estrutura inicial da pagina de posts

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,13 +1,47 @@
+import { cloneElement, ReactElement, useEffect, useState } from 'react'
 import { GetStaticProps, GetStaticPropsContext } from 'next'
+
+import { Heading, Stack, Text } from '@chakra-ui/react'
+import { Badge } from '../../components/Badge'
+import { FlexGap } from '../../components/FlexGap'
+
 import { notion } from '../../services/notion'
+import { parseBlocksToComponents } from '../../utils/NotionUtil'
 import { config } from '../../components/config'
 
+import { postsMock } from '../../json/postsMock'
+
 export default function Page({ post }: { post: any }) {
+  const [componentsJSX, setComponentsJSX] = useState<ReactElement[] | null>(null)
+
+  useEffect(() => {
+    if (post) {
+      const components = parseBlocksToComponents(post.body)
+      setComponentsJSX(components)
+    }
+  }, [post])
+
   return (
-    <>
-      <h1>{post?.title}</h1>
-      <code>{JSON.stringify(post, null, 4)}</code>
-    </>
+    <Stack width="100" maxWidth="900px" margin="40px auto" spacing={10}>
+      <Stack as="header" align="center">
+        <Heading as="h1" maxWidth="700px" textAlign="center">{post?.title}</Heading>
+        <Text>{post?.dateDisplay}</Text>
+
+        {post?.tags && (
+          <FlexGap>
+            {post?.tags.map((tag: string, i: number) => (
+              <Badge key={i} bg="gray.700">{tag}</Badge>
+            ))}
+          </FlexGap>
+        )}
+      </Stack>
+
+      <Stack as="article">
+        {componentsJSX?.map((Comp, idx) => {
+          return cloneElement(Comp, { key: idx })
+        })}
+      </Stack>
+    </Stack>
   )
 }
 
@@ -15,14 +49,16 @@ export const getStaticProps: GetStaticProps = async (context: GetStaticPropsCont
   try {
     const slug = context.params?.slug
 
-    if (typeof slug !== 'string') throw new TypeError()
+    if (typeof slug !== 'string') throw new TypeError('slug is not a string')
 
     const start = slug.lastIndexOf('-') + 1
     const end = slug.length
 
     const pageId = slug.slice(start, end)
 
-    const post = await notion.posts.getFullPost(pageId)
+    // const post = await notion.posts.getFullPost(pageId)
+
+    const post = postsMock
 
     return {
       props: { post },
