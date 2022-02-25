ddc9439573ae2faaa4d03c8cf5ab9b7b7a52f973
Vitor DevSP <contatodevsp@gmail.com>
2022-02-25 18:04:45 -0300

    feat: criando a estrutura de tipagem do bloco

--- src/services/notion/types/notion.types.ts
+++ src/services/notion/types/notion.types.ts
@@ -1,5 +1,7 @@
 /* eslint-disable camelcase */
 
+import { BulletedListItemType, CodeType, H1Type, H2Type, H3Type, ImageType, ParagraphType, PropTypeType } from './notionBlocks.types'
+
 export interface NotionDatabaseType {
   object: 'list'
   results: any[]
@@ -25,16 +27,52 @@ export interface NotionPageType {
   url: string
 }
 
-// TODO: concluir a criação da tipagem
 export interface NotionBlockType {
   object: 'block'
   id: string
-  archived: false
-  has_children: true
   created_time: string
   last_edited_time: string
-  type: 'child_page'
-  child_page?: {
-    title: string
+  has_children: boolean
+  archived: boolean
+  created_by: {
+    object: 'user'
+    id: string
   }
+  last_edited_by: {
+    object: 'user'
+    id: string
+  }
+  type: PropTypeType
+
+  paragraph?: ParagraphType
+  heading_1?: H1Type
+  heading_2?: H2Type
+  heading_3?: H3Type
+  bulleted_list_item?: BulletedListItemType
+  numbered_list_item?: any
+  code?: CodeType
+  to_do?: any
+  toggle?: any
+  child_page?: any
+  child_database?: any
+  embed?: any
+  image?: ImageType
+  video?: any
+  file?: any
+  pdf?: any
+  bookmark?: any
+  callout?: any
+  quote?: any
+  equation?: any
+  divider?: any
+  table_of_contents?: any
+  column?: any
+  column_list?: any
+  link_preview?: any
+  synced_block?: any
+  template?: any
+  link_to_page?: any
+  table?: any
+  table_row?: any
+  unsupported?: null
 }
