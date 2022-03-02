f92ba24fcd9e97356a742b7d22af1b764382e26a
Vitor DevSP <contatodevsp@gmail.com>
2022-03-02 18:06:43 -0300

    refactor(post): buscando o conteudo da API Route

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -5,31 +5,39 @@ import { Heading, Spinner, Stack, Text } from '@chakra-ui/react'
 import { Badge } from '../../components/Badge'
 import { FlexGap } from '../../components/FlexGap'
 
-import { notion } from '../../services/notion'
+import { api } from '../../services/api'
 import { parseBlocksToComponents } from '../../utils/NotionUtil'
+import { PostDataType } from '../api/notion/_resources/modules/posts/types/post.types'
+
 import { config } from '../../components/config'
 
-export default function Page({ post }: { post: any }) {
+interface PostProps {
+  post: PostDataType | null
+}
+
+export default function Post({ post }: PostProps) {
   const [componentsJSX, setComponentsJSX] = useState<ReactElement[] | null>(null)
 
   useEffect(() => {
-    if (post) {
-      const components = parseBlocksToComponents(post.body)
+    if (post?.data.body) {
+      const components = parseBlocksToComponents(post.data.body)
       setComponentsJSX(components)
     }
   }, [post])
 
-  if (!post) return <Spinner position="absolute" top="50%" left="50%" />
+  if (!post?.data.body) {
+    return <Spinner position="absolute" top="50%" left="50%" />
+  }
 
   return (
     <Stack width="100" maxWidth="900px" margin="40px auto" spacing={10}>
       <Stack as="header" align="center">
-        <Heading as="h1" maxWidth="700px" textAlign="center">{post?.title}</Heading>
-        <Text>{post?.dateDisplay}</Text>
+        <Heading as="h1" maxWidth="700px" textAlign="center">{post?.data.title}</Heading>
+        <Text>{post?.data.dateDisplay}</Text>
 
-        {post?.tags && (
+        {post?.data.tags && (
           <FlexGap justify="center">
-            {post?.tags.map((tag: string, i: number) => (
+            {post?.data.tags.map((tag: string, i: number) => (
               <Badge key={i} bg="gray.700">{tag}</Badge>
             ))}
           </FlexGap>
@@ -56,16 +64,22 @@ export const getStaticProps: GetStaticProps = async (context: GetStaticPropsCont
 
     const pageId = slug.slice(start, end)
 
-    const post = await notion.posts.getFullPost(pageId)
+    const { data: post } = await api.get<PostDataType>(`notion/posts/${pageId}`, {
+      params: {
+        body: true,
+      },
+    })
 
     return {
-      props: { post },
+      props: {
+        post,
+      },
       revalidate: config.revalidate,
     }
   } catch (e) {
     return {
       props: { post: null },
-      revalidate: config.revalidate,
+      revalidate: 1,
     }
   }
 }
