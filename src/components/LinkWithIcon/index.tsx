a558c7bc947b50d1fb035e46b8a8e87af218228e
vitordevsp <contatodevsp@gmail.com>
2023-12-10 20:48:43 -0300

    feat: cria o comp. LinkWithIcon e o obj socialMedia com os links das redes

--- src/components/LinkWithIcon/index.tsx
+++ src/components/LinkWithIcon/index.tsx
@@ -1,5 +1,20 @@
-export function LinkWithIcon() {
+import { Icon, IconName } from ".."
+import "./style.css"
+
+interface LinkWithIconProps {
+  href?: string
+  icon?: IconName
+}
+
+export function LinkWithIcon({ href, icon }: LinkWithIconProps) {
   return (
-    <div></div>
+    <a
+      className="link-with-icon"
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {icon && <Icon name={icon} color="var(--text-color-gray)" />}
+    </a>
   )
 }
