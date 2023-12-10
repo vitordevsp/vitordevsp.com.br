49a403584e1d8674b50d95ef257aaccd98e2a742
vitordevsp <contatodevsp@gmail.com>
2023-12-10 20:47:02 -0300

    feat: add lib react-icons e cria o comp. Icon

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,5 +1,28 @@
-export function Icon() {
+import {
+  FiGithub,
+  FiYoutube,
+  FiLinkedin,
+  FiInstagram,
+} from "react-icons/fi"
+import { IconBaseProps, IconType } from "react-icons"
+
+export type IconName = "github" | "youtube" | "linkedin" | "instagram"
+
+export interface IconProps extends IconBaseProps {
+  name: IconName
+}
+
+const iconObj: Record<IconName, IconType> = {
+  github: FiGithub,
+  youtube: FiYoutube,
+  linkedin: FiLinkedin,
+  instagram: FiInstagram,
+}
+
+export function Icon({ name, ...rest }: IconProps) {
+  const IconToRender = iconObj[name]
+
   return (
-    <div></div>
+    <IconToRender {...rest} />
   )
 }
