dcf47258404feb7c26ad7499baf60059526b49ff
vitordevsp <contatodevsp@gmail.com>
2023-12-10 22:20:52 -0300

    feat: cria comp. Tag

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -1,5 +1,16 @@
-export function Tag() {
+import { Span } from ".."
+import "./style.css"
+
+export interface TagProps {
+  text: string
+}
+
+export function Tag({ text }: TagProps) {
   return (
-    <div></div>
+    <div className="tag">
+      <Span size="sm">
+        {text}
+      </Span>
+    </div>
   )
 }
