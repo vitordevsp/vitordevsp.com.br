afe4e19231aa97786a708110f82176e0096639c9
vitordevsp <contatodevsp@gmail.com>
2023-12-22 23:05:04 -0300

    feat: implementa a pagina posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,51 @@
-import { PageContainer } from "@/components"
+import {
+  BlogPostCard,
+  Heading,
+  PageContainer,
+  Paragraph,
+} from "@/components"
 
 export default function PostsPage() {
   return (
-    <PageContainer>
-      <h1>Postagens</h1>
+    <PageContainer className="flex flex-col items-center py-20 gap-20">
+      <section className="flex flex-col gap-8 text-center max-w-3xl">
+        <div>
+          <Heading as="h1">
+            Todas as Postagens
+          </Heading>
+
+          <Paragraph>
+            3 Postagens
+          </Paragraph>
+        </div>
+
+        <Paragraph>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s.
+        </Paragraph>
+      </section>
+
+      <section className="flex flex-col gap-20">
+        <BlogPostCard
+          title="Padronização de código com (ESLint e EditorConfig)"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          date="07 de julho de 2021"
+          tags={["React JS", "React Native", "Next JS"]}
+        />
+
+        <BlogPostCard
+          title="Padronização de código com (ESLint e EditorConfig)"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          date="07 de julho de 2021"
+          tags={["React JS", "React Nativexxx", "Next JSxxx", "Vue JS"]}
+        />
+
+        <BlogPostCard
+          title="Padronização de código com (ESLint e EditorConfig)"
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          date="07 de julho de 2021"
+          tags={["React JS", "Next JS"]}
+        />
+      </section>
     </PageContainer>
   )
 }
