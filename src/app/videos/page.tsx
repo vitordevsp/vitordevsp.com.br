e0fac4611265e2a0ce87618654dd123fbeea6e1f
vitordevsp <contatodevsp@gmail.com>
2023-12-22 23:14:40 -0300

    feat: implementa a pagina videos

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -1,9 +1,48 @@
-import { PageContainer } from "@/components"
+import {
+  Heading,
+  PageContainer,
+  Paragraph,
+  VideoCard,
+} from "@/components"
 
 export default function VideosPage() {
   return (
-    <PageContainer>
-      <h1>Videos</h1>
+    <PageContainer className="flex flex-col items-center py-20 gap-20">
+      <section className="flex flex-col gap-8 text-center max-w-3xl">
+        <div>
+          <Heading as="h1">
+            Projetos autorais
+          </Heading>
+
+          <Paragraph>
+            2 Projetos
+          </Paragraph>
+        </div>
+
+        <Paragraph>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard dummy text ever since the 1500s.
+        </Paragraph>
+      </section>
+
+      <section className="flex flex-col gap-20 max-w-4xl">
+        <VideoCard
+          title="My Finances"
+          date="30 de junho de 2021"
+          tags={["ReactJS", "NextJS", "NodeJS", "MongoDB"]}
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          linkYoutube="https://www.my-finances.app"
+          linkPost="https://www.my-finances.app"
+        />
+
+        <VideoCard
+          title="My Finances"
+          date="30 de junho de 2021"
+          tags={["ReactJS", "NextJS", "NodeJS", "MongoDB"]}
+          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+          linkYoutube="https://www.my-finances.app"
+          linkPost="https://www.my-finances.app"
+        />
+      </section>
     </PageContainer>
   )
 }
--- src/components/VideoCard/index.tsx
+++ src/components/VideoCard/index.tsx
@@ -1,5 +1,50 @@
-export function VideoCard() {
+import { Heading, Icon, LinkWithIcon, Paragraph, Span, Tags } from ".."
+import "./style.css"
+
+export interface VideoCardProps {
+  title: string
+  description: string
+  date: string
+  tags?: string[]
+  linkYoutube?: string
+  linkPost?: string
+}
+
+export function VideoCard({
+  title,
+  description,
+  date,
+  tags,
+  linkYoutube,
+  linkPost,
+}: VideoCardProps) {
   return (
-    <div></div>
+    <div className="video-card flex gap-14">
+      <div className="fake-img-video"></div>
+
+      <div>
+        <Heading>
+          {title}
+        </Heading>
+
+        <div className="flex items-center gap-3">
+          <Icon name="calendar" size={14} />
+
+          <Span>{date}</Span>
+        </div>
+
+        <Tags items={tags} />
+
+        <Paragraph className="mt-2">
+          {description}
+        </Paragraph>
+
+        <div className="flex gap-6 mt-4">
+          {linkYoutube && <LinkWithIcon icon="youtube" href={linkYoutube} text="Link do Vídeo" />}
+
+          {linkPost && <LinkWithIcon icon="link" href={linkPost} text="Link do Post" />}
+        </div>
+      </div>
+    </div>
   )
 }
