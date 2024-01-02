158a31a0bddfa790823ad1d7ffde595f786fb0bb
vitordevsp <contatodevsp@gmail.com>
2024-01-01 22:54:53 -0300

    refactor: comp. Header style scss

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 
 import { ActiveLink } from "./components/ActiveLink"
-import "./style.css"
+import "./style.scss"
 
 export function Header() {
   return (
@@ -8,10 +8,10 @@ export function Header() {
       <nav>
         <ul>
           <ActiveLink href="/" text="Home" />
-          <ActiveLink href="/sobre" text="Sobre" />
           <ActiveLink href="/projetos" text="Projetos" />
           <ActiveLink href="/posts" text="Posts" />
           <ActiveLink href="/videos" text="Vídeos" />
+          <ActiveLink href="/sobre" text="Sobre" />
         </ul>
       </nav>
     </header>
